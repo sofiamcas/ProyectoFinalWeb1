21f3baf673d38943a1b4b1e22d22d8819e1d0cc2
sofiamcas <119833805+sofiamcas@users.noreply.github.com>
2024-05-21 20:13:48 -0600

    Delete ProyectoFinalWeb1 directory

--- ProyectoFinalWeb1/delete.php
+++
@@ -1,8 +0,0 @@
-<?php
-  include 'conexion.php';
-  $id = $_GET['id'];
-  $sql = "delete from usuarios where id=$id";
-  $conexion->query($sql);
-  $conexion->close();
-  header("location: home.php");
-  
